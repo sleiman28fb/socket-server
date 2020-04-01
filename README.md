59b7153fc9e64f98406cb2e92d7063902ce3dba6
sleiman28fb <sleiman28fb@gmail.com>
2020-04-01 11:36:26 -0600

    primer commit - rest server

---
+++ README.md
@@ -0,0 +1,19 @@
+# Socket-Server
+
+
+reconstruir modulos de node
+```
+npm install
+````
+
+generar el dis
+`````
+tsc -w
+`````
+
+levantar el servidor , cualquiera de los comandos
+``````
+nodemon dist/
+node dist/
+````
+
---
+++ index.ts
@@ -0,0 +1,22 @@
+import Server from './clases/server';
+import router from './routes/router';
+import bodyParser from 'body-parser';
+import cors from 'cors'
+
+const server = new Server();
+
+
+
+//body parser
+server.app.use(bodyParser.urlencoded({extended:true}));
+server.app.use(bodyParser.json());
+
+//cors
+server.app.use(cors({origin: true, credentials:true}) );
+
+//Rutas de servicios
+server.app.use('/',router);
+
+server.start(()=>{
+   console.log(`Servidor esta corriendo en el puerto ${server.port}`);
+});
